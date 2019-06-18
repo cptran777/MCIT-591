54712b9f2220832832db6ddec575d4aab6c6526d
cptran777 <catran@linkedin.com>
2019-06-17 19:18:13 -0700

    Trying to create a conflict

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Nice to meet you!");
+    System.out.println("This should create a conflict");
   }
 }
